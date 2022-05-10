dbb529382896574e38e8df92b7c07ec8b4db2bcb
wiktoriakupranska <91828963+wiktoriakupranska@users.noreply.github.com>
2022-05-10 17:20:47 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,1 @@
-variable "path" {
-	type = list(object({
-	module = string
-	}))
-	default = [
-		{
-		module = "./"
-		}]
-	}
-variable "python-funcjonality" {
-	type = string
-	default = "print('Hello World')"
-}
+
